4e9e17574b419e535e2a729a66e1459a97483a3c
Amit prakash <amitstudy195@gmail.com>
2026-07-03 17:25:07 +0530

    cors problem solve

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,7 +13,7 @@ const sendTokenResponse = (user, statusCode, res) => {
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: 'none',
   };
 
   res
